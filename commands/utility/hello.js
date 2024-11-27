a110ffb54fd7ee95e66eeef148ed414c33fef4ef
AlexandreFrancony <alexandre.francony05@gmail.com>
2024-11-27 17:54:36 +0100

    French update

--- commands/utility/hello.js
+++ commands/utility/hello.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('hello')
-		.setDescription('Say hi to Bisitgri!'),
+		.setDescription('Dis bonjour à Bisitgri!'),
 	async execute(interaction) {
 		await interaction.reply(`Hello, ${interaction.user.username} ! 👋`);
 	},
