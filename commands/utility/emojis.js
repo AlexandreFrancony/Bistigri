1b370888a3971efe42961012f8a773ffc92fb2a7
AlexandreFrancony <alexandre.francony05@gmail.com>
2024-12-02 16:45:33 +0100

    fix emojis

--- commands/utility/emojis.js
+++ commands/utility/emojis.js
@@ -1,4 +1,4 @@
 module.exports = {
-    playin: "<:1313152450634453105>",
-    scryfall: "<:1313152435572834324>"
+    playin: "<:playin:1313152450634453105>",
+    scryfall: "<:scryfall:1313152435572834324>"
 };
