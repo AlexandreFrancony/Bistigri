b5fe498e2a83dac0658a697306ebefcb69310f16
AlexandreFrancony <alexandre.francony05@gmail.com>
2024-12-02 16:20:19 +0100

    add activity

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		client.user.setActivity('MTG', { type: 'PLAYING' });
 		console.log(`${client.user.tag} est maintenant en ligne !`);
 	},
 };
